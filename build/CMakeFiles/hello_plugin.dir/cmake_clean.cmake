87b849147ec5ee87d0ab4816406ef10269c7ca62
jwShalom <samuelseo15@gmail.com>
2023-07-17 16:08:27 +0900

    My Gazebo World

---
+++ build/CMakeFiles/hello_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_plugin.dir/script/hello_plugin.cpp.o"
+  "libhello_plugin.pdb"
+  "libhello_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
